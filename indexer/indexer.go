ec1320f8839e3fedc822008156ca5496785f28de
Andreas Koch <andyk7@gmail.com>
2013-01-27 22:33:25 +0100

    Use the Date type for the documents creation date

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -2,6 +2,7 @@ package indexer
 
 import (
 	"andyk/docs/model"
+	"time"
 )
 
 func Index() model.Document {
@@ -12,6 +13,7 @@ func Index() model.Document {
 	doc.Description = "Description"
 	doc.Content = "Content"
 	doc.Language = "en-US"
+	doc.Date = time.Date(2013, 1, 13, 0, 0, 0, 0, time.UTC)
 
 	return doc
 }
--- model/model.go
+++ model/model.go
@@ -4,11 +4,13 @@
 */
 package model
 
+import "time"
+
 type Document struct {
-	Path        string // The documents folder
-	Title       string // The document title
-	Description string // A short description of the document content.
-	Content     string // The document content
-	Language    string // [optional] The ISO language code document (e.g. "en-GB", "de-DE")
-	Date        string // [optional] The date the document has been created
+	Path        string    // The documents folder
+	Title       string    // The document title
+	Description string    // A short description of the document content.
+	Content     string    // The document content
+	Language    string    // [optional] The ISO language code document (e.g. "en-GB", "de-DE")
+	Date        time.Time // [optional] The date the document has been created
 }
