02f978f5897c5bf58fdc44ba7365634790b93830
Andreas Koch <andyk7@gmail.com>
2013-01-29 20:36:49 +0100

    Added a script which provides and auto-build service. Once you run the script 'start-buildserver.sh' you will have an up-to-date version of the application in your bin directory (Credits: https://exyr.org/2011/inotify-run/. Requirements: https://github.com/rvoicilas/inotify-tools).

---
+++ start-buildservice.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Starts a filesystem-wachter which compiles the
+# the application everytime a file gets changes
+# in the current folder.
+# source: https://exyr.org/2011/inotify-run/
+
+# Requirements:
+# - inotify-tools (https://github.com/rvoicilas/inotify-tools)
+
+# filename: start-buildservice.sh
+
+FORMAT=$(echo -e "\033[1;33m%w%f\033[0m written")
+"$@"
+while inotifywait -qre close_write --format "$FORMAT" .
+do
+    "$@"
+    go install server/server.go
+done
