e45b06e1971fd110c221e810ed64263d688f6191
Andreas Koch <andyk7@gmail.com>
2013-02-17 21:47:10 +0100

    Created first unit tests for the ParseIso8601Date function of the date package

---
+++ date/date_test.go
@@ -0,0 +1,58 @@
+package date
+
+import (
+	"testing"
+)
+
+func Test_ParseIso8601Date_ValidIso8601Dates_NoErrorIsReturned(t *testing.T) {
+
+	// Arrange
+	dateStrings := []string{
+		"2013-02-08",
+		"2013-01-01",
+		"2013-12-31",
+		"0001-01-01",
+		"0001-12-31",
+		"9999-01-01",
+		"9999-12-31",
+	}
+
+	// Act
+	for _, dateString := range dateStrings {
+		_, err := ParseIso8601Date(dateString)
+
+		// Assert
+		if err != nil {
+			t.Fail()
+			t.Logf("Parsing the value '%v' returned an error even though no error was expected.", dateString)
+		}
+
+	}
+}
+
+func Test_ParseIso8601Date_InvalidIso8601Dates_ErrorIsReturned(t *testing.T) {
+
+	// Arrange
+	dateStrings := []string{
+		"99-02-08",
+		"1-1-1",
+		"2013-1-1",
+		"2013-01-1",
+		"2013-1-01",
+		"13-01-01",
+		"83-12-31",
+		"21400-12-31",
+	}
+
+	// Act
+	for _, dateString := range dateStrings {
+		_, err := ParseIso8601Date(dateString)
+
+		// Assert
+		if err == nil {
+			t.Fail()
+			t.Logf("Parsing the value '%v' returned should return an error because it is not a valid date.", dateString)
+		}
+
+	}
+}
