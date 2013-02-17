22925e37ea0407e3c23d19e94dd9e406a29e694c
Andreas Koch <andyk7@gmail.com>
2013-02-17 11:38:04 +0100

    Added missing code comments to the Document types and functions

--- model/Document.go
+++ model/Document.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"log"
 	"regexp"
 	"strings"
 )
@@ -16,6 +17,7 @@ type Document struct {
 	rawLines []string
 }
 
+// CreateDocument returns a new Document from the given RepositoryItem.
 func CreateDocument(repositoryItem *RepositoryItem) *Document {
 	doc := Document{
 		Hash:     repositoryItem.GetHash(),
@@ -27,6 +29,7 @@ func CreateDocument(repositoryItem *RepositoryItem) *Document {
 	return doc.parse()
 }
 
+// getLastElement retrn the last element of a string array.
 func getLastElement(array []string) string {
 	if array == nil {
 		return ""
@@ -35,10 +38,14 @@ func getLastElement(array []string) string {
 	return array[len(array)-1]
 }
 
+// parse starts the parsing of the current document.
+// All regognized blocks will be assigned.
 func (doc *Document) parse() *Document {
 	return doc.setTitle()
 }
 
+// setTitle checks if the current Document
+// contains a title and if yes, assigns it.
 func (doc *Document) setTitle() *Document {
 	titleLocation := doc.locateTitle()
 	if !titleLocation.Found {
@@ -49,6 +56,8 @@ func (doc *Document) setTitle() *Document {
 	return doc.setDescription()
 }
 
+// setDescription checks if the current Document
+// contains a description block and if yes, assigns it.
 func (doc *Document) setDescription() *Document {
 	descriptionLocation := doc.locateDescription()
 	if !descriptionLocation.Found {
@@ -59,6 +68,8 @@ func (doc *Document) setDescription() *Document {
 	return doc.setContent()
 }
 
+// setContent checks if the current Document
+// contains a content block and if yes, assigns it.
 func (doc *Document) setContent() *Document {
 	contentLocation := doc.locateContent()
 	if !contentLocation.Found {
@@ -69,6 +80,8 @@ func (doc *Document) setContent() *Document {
 	return doc.setMetaData()
 }
 
+// setMetaData checks if the current Document
+// does contain meta data and if yes, assigns it.
 func (doc *Document) setMetaData() *Document {
 	metaDataLocation := doc.locateMetaData()
 	if !metaDataLocation.Found {
@@ -79,6 +92,8 @@ func (doc *Document) setMetaData() *Document {
 	return doc
 }
 
+// DocumentPattern contains a set of regular expression
+// for parsing documents.
 type DocumentPattern struct {
 	EmptyLine      regexp.Regexp
 	Title          regexp.Regexp
@@ -87,11 +102,24 @@ type DocumentPattern struct {
 	MetaData       regexp.Regexp
 }
 
+// NewDocumentPattern returns a new DocumentPattern
+// for parsing documents.
 func NewDocumentPattern() DocumentPattern {
+	// Lines which contain nothing but white space characters
+	// or no characters at all.
 	emptyLineRegexp := regexp.MustCompile("^\\s*$")
-	titleRegexp := regexp.MustCompile("\\s*#\\s*(.+)")
+
+	// Lines which a start with a hash, followed by zero or more
+	// white space characters, followed by text.
+	titleRegexp := regexp.MustCompile("\\s*#\\s*(\\w.+)")
+
+	// Lines which start with text
 	descriptionRegexp := regexp.MustCompile("^\\w.+")
+
+	// Lines which nothing but dashes
 	horizontalRuleRegexp := regexp.MustCompile("^-{2,}")
+
+	// Lines with a "key: value" syntax
 	metaDataRegexp := regexp.MustCompile("^(\\w+):\\s*(\\w.+)$")
 
 	return DocumentPattern{
@@ -103,45 +131,63 @@ func NewDocumentPattern() DocumentPattern {
 	}
 }
 
-type LineSet struct {
+// LineRange contains a Start- and a End line number.
+type LineRange struct {
 	Start int
 	End   int
 }
 
-func NewLineSet(start int, end int) LineSet {
-	return LineSet{
+// NewLineRange returns a new LineRange
+// with the given start and end.
+func NewLineRange(start int, end int) LineRange {
+	if start < 0 || end < 0 || (start > end) {
+		log.Panicf("Invalid start and end values for a LineRange. Start: %v, End: %v", start, end)
+	}
+
+	return LineRange{
 		Start: start,
 		End:   end,
 	}
 }
 
+// A MatchResult represents the result of a pattern matching
+// process on the content of an document.
+// It indicates whether the pattern was found and if yet,
+// the lines in which it was located and the matched text.
 type MatchResult struct {
 	Found   bool
-	Lines   LineSet
+	Lines   LineRange
 	Matches []string
 }
 
+// Found create a new MatchResult which represents
+// a successful match.
 func Found(firstLine int, lastLine int, matches []string) *MatchResult {
 	return &MatchResult{
 		Found:   true,
-		Lines:   NewLineSet(firstLine, lastLine),
+		Lines:   NewLineRange(firstLine, lastLine),
 		Matches: matches,
 	}
 }
 
+// NotFound create a new MatchResult which represents
+// an unsuccessful match.
 func NotFound() *MatchResult {
 	return &MatchResult{
 		Found: false,
-		Lines: NewLineSet(-1, -1),
+		Lines: NewLineRange(-1, -1),
 	}
 }
 
-func IsMatch(line string, pattern regexp.Regexp) (isMatch bool, matches []string) {
-	matches = pattern.FindStringSubmatch(line)
+// IsMatch returns a flag indicating whether the supplied
+// text and pattern do match and if yet, the matched text.
+func IsMatch(text string, pattern regexp.Regexp) (isMatch bool, matches []string) {
+	matches = pattern.FindStringSubmatch(text)
 	return matches != nil, matches
 }
 
-// Check if the current Document contains a title
+// locateTitle checks if the current Document
+// contains a title.
 func (doc *Document) locateTitle() *MatchResult {
 
 	// In order to be the "title" the line must either
@@ -163,7 +209,8 @@ func (doc *Document) locateTitle() *MatchResult {
 	return NotFound()
 }
 
-// Check if the current Document contains a description
+// locateDescription checks if the current Document
+// contains a description.
 func (doc *Document) locateDescription() *MatchResult {
 
 	// The description must be preceeded by a title
@@ -199,7 +246,8 @@ func (doc *Document) locateDescription() *MatchResult {
 	return NotFound()
 }
 
-// Check if the current Document contains meta data
+// locateMetaData checks if the current Document
+// contains meta data.
 func (doc *Document) locateMetaData() *MatchResult {
 
 	// Find the last horizontal rule in the document
@@ -248,7 +296,8 @@ func (doc *Document) locateMetaData() *MatchResult {
 	return NotFound()
 }
 
-// Check if the current Document contains content
+// locateContent checks if the current Document
+// contains content.
 func (doc *Document) locateContent() *MatchResult {
 
 	// Content must be preceeded by a description
