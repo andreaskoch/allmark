fb7faaa76299e809f9aae8ca97a71e6a1265587e
Andreas Koch <andyk7@gmail.com>
2013-02-23 13:29:10 +0100

    Removed the GetRenderedItemHash function, because for now I really don't need the functionality. I might add it back later.

--- repository/parser.go
+++ repository/parser.go
@@ -1,10 +1,7 @@
 package repository
 
 import (
-	"bufio"
-	"os"
 	"path/filepath"
-	"regexp"
 )
 
 func GetRenderer(repositoryItem *Item) func() {
@@ -21,36 +18,6 @@ func GetParser(repositoryItem *Item) func() {
 
 	}
 
-}
-
-// Get the hash code of the rendered item
-func GetRenderedItemHash(item Item) string {
-	renderedItemPath := GetRenderedItemPath(item)
-
-	file, err := os.Open(renderedItemPath)
-	if err != nil {
-		// file does not exist or cannot be accessed
-		return ""
-	}
-	defer file.Close()
-
-	fileReader := bufio.NewReader(file)
-	firstLineBytes, _ := fileReader.ReadBytes('\n')
-	if firstLineBytes == nil {
-		// first line cannot be read
-		return ""
-	}
-
-	// extract hash from line
-	hashCodeRegexp := regexp.MustCompile("<!-- (\\w+) -->")
-	matches := hashCodeRegexp.FindStringSubmatch(string(firstLineBytes))
-	if len(matches) != 2 {
-		return ""
-	}
-
-	extractedHashcode := matches[1]
-
-	return string(extractedHashcode)
 }
 
 // Get the filepath of the rendered repository item
