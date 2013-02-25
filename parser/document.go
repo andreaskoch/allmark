46db78f7768cb88ef2304c64939b6d8290f78884
Andreas Koch <andyk7@gmail.com>
2013-02-25 21:27:37 +0100

    Moved the getMatchingValue and getContent functions from the document to the parser since these functions are not specific to the document parsing

--- parser/document.go
+++ parser/document.go
@@ -1,12 +1,5 @@
 package parser
 
-import (
-	"andyk/docs/util"
-	"github.com/russross/blackfriday"
-	"regexp"
-	"strings"
-)
-
 type DocumentParser struct {
 	Patterns DocumentStructure
 }
@@ -23,57 +16,15 @@ func (parser DocumentParser) Parse(lines []string, metaData MetaData) (item Pars
 	item.MetaData = metaData
 
 	// title
-	title, lines := parser.getMatchingValue(lines, parser.Patterns.Title)
+	title, lines := getMatchingValue(lines, parser.Patterns.Title, parser.Patterns.EmptyLine)
 	item.AddElement("title", title)
 
 	// description
-	description, lines := parser.getMatchingValue(lines, parser.Patterns.Description)
+	description, lines := getMatchingValue(lines, parser.Patterns.Description, parser.Patterns.EmptyLine)
 	item.AddElement("description", description)
 
 	// content
-	item.AddElement("content", parser.getContent(lines))
+	item.AddElement("content", getContent(lines))
 
 	return item, nil
-}
-
-func (parser DocumentParser) getMatchingValue(lines []string, pattern regexp.Regexp) (string, []string) {
-
-	// In order to be the "matching value" the line must
-	// either be empty or match the supplied pattern.
-	for lineNumber, line := range lines {
-
-		lineMatchesTitlePattern, matches := util.IsMatch(line, pattern)
-		if lineMatchesTitlePattern {
-			nextLine := getNextLinenumber(lineNumber, lines)
-			return util.GetLastElement(matches), lines[nextLine:]
-		}
-
-		lineIsEmpty := parser.Patterns.EmptyLine.MatchString(line)
-		if !lineIsEmpty {
-			break
-		}
-	}
-
-	return "", lines
-}
-
-func (parser DocumentParser) getContent(lines []string) string {
-
-	// all remaining lines are the (raw markdown) content
-	rawMarkdownContent := strings.TrimSpace(strings.Join(lines, "\n"))
-
-	// html encode the markdown
-	htmlEncodedContent := string(blackfriday.MarkdownBasic([]byte(rawMarkdownContent)))
-
-	return htmlEncodedContent
-}
-
-func getNextLinenumber(lineNumber int, lines []string) int {
-	nextLine := lineNumber + 1
-
-	if nextLine <= len(lines) {
-		return nextLine
-	}
-
-	return lineNumber
 }
--- parser/parser.go
+++ parser/parser.go
@@ -5,8 +5,10 @@ import (
 	"andyk/docs/util"
 	"errors"
 	"fmt"
+	"github.com/russross/blackfriday"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
@@ -114,4 +116,46 @@ func getItemTypeFromFilename(filename string) string {
 	}
 
 	return "unknown"
+}
+
+func getMatchingValue(lines []string, matchPattern regexp.Regexp, emptyLinePattern regexp.Regexp) (string, []string) {
+
+	// In order to be the "matching value" the line must
+	// either be empty or match the supplied pattern.
+	for lineNumber, line := range lines {
+
+		lineMatchesTitlePattern, matches := util.IsMatch(line, matchPattern)
+		if lineMatchesTitlePattern {
+			nextLine := getNextLinenumber(lineNumber, lines)
+			return util.GetLastElement(matches), lines[nextLine:]
+		}
+
+		lineIsEmpty := emptyLinePattern.MatchString(line)
+		if !lineIsEmpty {
+			break
+		}
+	}
+
+	return "", lines
+}
+
+func getContent(lines []string) string {
+
+	// all remaining lines are the (raw markdown) content
+	rawMarkdownContent := strings.TrimSpace(strings.Join(lines, "\n"))
+
+	// html encode the markdown
+	htmlEncodedContent := string(blackfriday.MarkdownBasic([]byte(rawMarkdownContent)))
+
+	return htmlEncodedContent
+}
+
+func getNextLinenumber(lineNumber int, lines []string) int {
+	nextLine := lineNumber + 1
+
+	if nextLine <= len(lines) {
+		return nextLine
+	}
+
+	return lineNumber
 }
