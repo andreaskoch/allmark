84e0f308b713acca15876745873f35d7feeb16e9
Andreas Koch <andyk7@gmail.com>
2013-06-18 21:19:23 +0200

    Use the "HARD_LINE_BREAK" extension for the blackfriday markdown renderer. This will translate new lines into line breaks.

--- markdown/markdown.go
+++ markdown/markdown.go
@@ -11,7 +11,27 @@ import (
 )
 
 func ToHtml(markdown string) (html string) {
-	return string(blackfriday.MarkdownCommon([]byte(markdown)))
+
+	// set up the HTML renderer
+	htmlFlags := 0
+	htmlFlags |= blackfriday.HTML_USE_XHTML
+	htmlFlags |= blackfriday.HTML_USE_SMARTYPANTS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_FRACTIONS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
+	htmlFlags |= blackfriday.HTML_SKIP_SCRIPT
+	renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
+
+	// set up the parser
+	extensions := 0
+	extensions |= blackfriday.EXTENSION_NO_INTRA_EMPHASIS
+	extensions |= blackfriday.EXTENSION_TABLES
+	extensions |= blackfriday.EXTENSION_FENCED_CODE
+	extensions |= blackfriday.EXTENSION_AUTOLINK
+	extensions |= blackfriday.EXTENSION_STRIKETHROUGH
+	extensions |= blackfriday.EXTENSION_SPACE_HEADERS
+	extensions |= blackfriday.EXTENSION_HARD_LINE_BREAK
+
+	return string(blackfriday.Markdown([]byte(markdown), renderer, extensions))
 }
 
 func IsMarkdownFile(fileNameOrPath string) bool {
