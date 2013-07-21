andreaskoch/allmark

commit 1c08413a0f41d9efb48370ee0576f67f3cff8de6

Created a footer section which contains a link to the sitemap and later maybe other 'footer'-links

templates/default.go
@@ -53,8 +53,16 @@ var masterTemplate = fmt.Sprintf(`<!DOCTYPE HTML>
 {{end}}
 
 <article class="{{.Type}} level-{{.Level}}">
-%s
+@childtemplate
 </article>
+
+<footer>
+	<nav>
+		<ul>
+			<li><a href="/sitemap.html">Sitemap</a></li>
+		</ul>
+	</nav>
+</footer>
 
 <script src="/theme/jquery.js"></script>
 <script src="/theme/autoupdate.js"></script>

themes/themefiles/printcss.go
@@ -21,14 +21,6 @@ body {
     outline: none;
 }
 
-body>nav.toplevel {
-    display: none;
-}
-
-body>nav.breadcrumb {
-    display: none;
-}
-
 a, a:visited {
     font-weight: normal;
     text-decoration: none;
@@ -81,6 +73,18 @@ h2, h3, h4, h5, h6 {
     page-break-after: avoid;
 }
 
+body>nav.toplevel {
+    display: none;
+}
+
+body>nav.breadcrumb {
+    display: none;
+}
+
+body>footer {
+    display: none;
+}
+
 .pdf .metadata {
     display: none;
 }
@@ -107,5 +111,4 @@ h2, h3, h4, h5, h6 {
 
 .presentation nav {
     display: none;
-}
-`
+}`

themes/themefiles/screencss.go
@@ -26,69 +26,6 @@ body {
     outline: 1300px solid #FAFAFA;
 }
 
-body>nav.toplevel {
-    text-align: right;
-    font-size: 1.1em;
-    margin: 0 0 15px 0;
-    padding: 0;
-    clear: both;
-}
-
-body>nav.toplevel>ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-}
-
-body>nav.toplevel>ul>li {
-    display: inline-block;
-    border: 1px solid #000000;
-    background-color: #FFFFFF;
-    margin: -1px -5px 0 -2px;
-    padding: 0px 10px;
-    white-space: nowrap;
-}
-
-body>nav.toplevel>ul>li:last-child {
-    border-right: 1px solid #000000;
-    clear: both;
-}
-
-body>nav.toplevel>ul>li>a {
-    font-family: "Helvetia", "Verdana", "Sans-Serif";
-    color: #000000;
-}
-
-body>nav.toplevel>ul>li>a:hover {
-    color: #06e;
-}
-
-body>nav.breadcrumb {
-    font-size: 0.8em;
-}
-
-body>nav.breadcrumb>ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-}
-
-body>nav.breadcrumb>ul>li {
-    display: inline;
-}
-
-body>nav.breadcrumb>ul>li:after {
-    content: "»";
-}
-
-body>nav.breadcrumb>ul>li:last-child:after {
-    content: "";
-}
-
-body>nav.breadcrumb>ul>li>a {
-    font-family: "Helvetia", "Verdana", "Sans-Serif";
-}
-
 a {
     color: #0645ad;
     text-decoration: none;
@@ -290,6 +227,89 @@ table {
 
 td {
     vertical-align: top;
+}
+
+body>footer {
+    margin: 25px 0 0 0;
+    border-top: 1px solid #eee;
+}
+
+body>footer>nav {
+    text-align: right;
+}
+
+body>footer>nav>ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+body>footer>nav>ul>li>a {
+    color: #000000;
+    font-size: 0.8em;
+}
+
+body>nav.toplevel {
+    text-align: right;
+    font-size: 1.1em;
+    margin: 0 0 15px 0;
+    padding: 0;
+    clear: both;
+}
+
+body>nav.toplevel>ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+body>nav.toplevel>ul>li {
+    display: inline-block;
+    border: 1px solid #000000;
+    background-color: #FFFFFF;
+    margin: -1px -5px 0 -2px;
+    padding: 0px 10px;
+    white-space: nowrap;
+}
+
+body>nav.toplevel>ul>li:last-child {
+    border-right: 1px solid #000000;
+    clear: both;
+}
+
+body>nav.toplevel>ul>li>a {
+    font-family: "Helvetia", "Verdana", "Sans-Serif";
+    color: #000000;
+}
+
+body>nav.toplevel>ul>li>a:hover {
+    color: #06e;
+}
+
+body>nav.breadcrumb {
+    font-size: 0.8em;
+}
+
+body>nav.breadcrumb>ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+body>nav.breadcrumb>ul>li {
+    display: inline;
+}
+
+body>nav.breadcrumb>ul>li:after {
+    content: "»";
+}
+
+body>nav.breadcrumb>ul>li:last-child:after {
+    content: "";
+}
+
+body>nav.breadcrumb>ul>li>a {
+    font-family: "Helvetia", "Verdana", "Sans-Serif";
 }
 
 article>.description {

commit a268342441ec0146fa133a6b71887bd4bcfd4a13

Fixed bug in the master template which has been introduced with commit 1c08413. The childtemplate placeholder must be added by fmt.Sprintf

templates/default.go
@@ -53,7 +53,7 @@ var masterTemplate = fmt.Sprintf(`<!DOCTYPE HTML>
 {{end}}
 
 <article class="{{.Type}} level-{{.Level}}">
-@childtemplate
+%s
 </article>
 
 <footer>