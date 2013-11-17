50921e4b6ea1b37ff82217dfde5418045a2f8d64
Andreas Koch <andyk7@gmail.com>
2013-11-17 17:03:48 +0100

    Squashed commit of the following:
    commit 9e7ea0375bc4e4268d5a0752ee1b2e0c860457ca
    Author: Andreas Koch <andyk7@gmail.com>
    Date:   Sun Nov 17 15:05:09 2013 +0100
    
        Files Extension: Link files
    
    commit c21a0402088fafcf794305997bc69cf5695bde37
    Author: Andreas Koch <andyk7@gmail.com>
    Date:   Sun Nov 17 14:50:55 2013 +0100
    
        Files Extension: Code cleanup
    
    commit b1d55f97760faab6ca9ae64caf224abc9e4831f3
    Author: Andreas Koch <andyk7@gmail.com>
    Date:   Sun Nov 17 14:37:43 2013 +0100
    
        Files Extension: URL Decode file names.
    
    commit 047ff9f31dabc41283ebe7f4d7f0549225ab263b
    Author: Andreas Koch <andyk7@gmail.com>
    Date:   Sun Nov 17 14:27:06 2013 +0100
    
        Improved the file link rendering. Don't print the name of the
        "files"-folder.
    
    commit 7fd2e74d19f8d0fda83453975b76a92e9f7430db
    Author: Andreas Koch <andyk7@gmail.com>
    Date:   Sun Nov 17 12:17:01 2013 +0100
    
        A somewhat working version of the files extension
    
    commit faf35cb43178bf09181e1e6e6db81e0e18fa41a1
    Author: Andreas Koch <andyk7@gmail.com>
    Date:   Thu Oct 10 23:02:20 2013 +0200
    
        File preview refactoring: First draft

--- converter/html/filelinks.go
+++ converter/html/filelinks.go
@@ -34,16 +34,14 @@ func renderFileLinks(fileIndex *repository.FileIndex, pathProvider *path.Provide
 
 		// create link list code
 		files := fileIndex.FilesByPath(path, allFiles)
+		filePaths := getFilePaths(title, files, pathProvider)
+		rootFolderTitle := getLastPathComponent(path)
+		rootFolder := NewRootFilesystemEntry(rootFolderTitle)
+		rootFolder = getFileSystemFromLinks(rootFolder, filePaths)
 
-		fileLinks := getFileLinks(title, files, pathProvider)
-		fileLinksCode := fmt.Sprintf(`<section class="filelinks">
-				<h1>%s</h1>
-				<ol>
-					<li>
-					%s
-					</li>
-				</ol>
-			</section>`, title, strings.Join(fileLinks, "\n</li>\n<li>\n"))
+		fileLinksCode := fmt.Sprintf(`<section class="filelinks"><h1>%s</h1>`, title)
+		fileLinksCode += renderFilesystemEntry(rootFolder, 0)
+		fileLinksCode += `</section>`
 
 		// replace markdown with link list
 		markdown = strings.Replace(markdown, originalText, fileLinksCode, 1)
@@ -53,18 +51,140 @@ func renderFileLinks(fileIndex *repository.FileIndex, pathProvider *path.Provide
 	return markdown
 }
 
-func getFileLinks(title string, files []*repository.File, pathProvider *path.Provider) []string {
+func getLastPathComponent(path string) string {
+	if !strings.Contains(path, "/") {
+		return path
+	}
+
+	components := strings.Split(path, "/")
+	return components[len(components)-1]
+}
+
+func renderFilesystemEntry(fsEntry *FileSystemEntry, level int) string {
+
+	html := ""
+
+	// don't print the "root-folder" name (-> "files")
+	if level > 0 {
+
+		// assemble the file or folder link
+		href := fsEntry.Path()
+		title := fsEntry.Name()
+		name := fsEntry.Name()
+
+		if fsEntry.IsDirectory() {
+			html = fmt.Sprintf(`%s`, name)
+		} else {
+			html = fmt.Sprintf(`<a href="%s" title="%s">%s</a>`, href, title, name)
+		}
+	}
+
+	// recurse if the filesystem entry has childs
+	if len(fsEntry.Childs) > 0 {
+
+		html += "<ul>\n"
+		for _, child := range fsEntry.Childs {
+			html += fmt.Sprintf("<li>%s</li>\n", renderFilesystemEntry(child, level+1))
+		}
+		html += "</ul>\n"
+
+	}
+
+	return html
+}
+
+type FileSystemEntry struct {
+	name   string
+	parent *FileSystemEntry
+	Childs []*FileSystemEntry
+}
+
+func NewRootFilesystemEntry(name string) *FileSystemEntry {
+	return &FileSystemEntry{
+		name:   name,
+		Childs: make([]*FileSystemEntry, 0),
+	}
+}
+
+func NewFilesystemEntry(parent *FileSystemEntry, name string) *FileSystemEntry {
+	return &FileSystemEntry{
+		name:   name,
+		parent: parent,
+		Childs: make([]*FileSystemEntry, 0),
+	}
+}
+
+func (fsEntry *FileSystemEntry) Path() string {
+	path := fsEntry.name
+
+	if fsEntry.Parent() != nil {
+		path = fsEntry.Parent().Path() + "/" + path
+	}
+
+	return path
+}
+
+func (fsEntry *FileSystemEntry) IsDirectory() bool {
+	return len(fsEntry.Childs) > 0
+}
+
+func (fsEntry *FileSystemEntry) Parent() *FileSystemEntry {
+	return fsEntry.parent
+}
+
+func (fsEntry *FileSystemEntry) Name() string {
+	return util.DecodeUrl(fsEntry.name)
+}
+
+func (fsEntry *FileSystemEntry) GetChild(name string) *FileSystemEntry {
+	for _, entry := range fsEntry.Childs {
+		if entry.name == name {
+			return entry
+		}
+	}
+
+	return nil
+}
+
+func getFileSystemFromLinks(root *FileSystemEntry, filePaths []string) *FileSystemEntry {
+
+	for _, filePath := range filePaths {
+
+		current := root
+
+		// split the path into components
+		components := strings.Split(filePath, "/")
+
+		// strip "files" component
+		if len(components) > 1 {
+			components = components[1:]
+		}
+
+		// trim empty components
+		components = util.TrimSlice(components)
+
+		// get the childs for each component
+		for _, component := range components {
+			child := current.GetChild(component)
+			if child == nil {
+				current.Childs = append(current.Childs, NewFilesystemEntry(current, component))
+				child = current.GetChild(component)
+			}
+			current = child
+		}
+	}
+
+	return root
+}
+
+func getFilePaths(title string, files []*repository.File, pathProvider *path.Provider) []string {
 
 	numberOfFiles := len(files)
 	fileLinks := make([]string, numberOfFiles, numberOfFiles)
 
 	for index, file := range files {
-
 		filePath := pathProvider.GetWebRoute(file)
-		fileTitle := fmt.Sprintf("%s - %s (File %v of %v)", title, getFileTitle(file), index+1, numberOfFiles)
-		linkText := getLinkTextFromFilePath(filePath)
-
-		fileLinks[index] = fmt.Sprintf(`<a href="%s" title="%s">%s</a>`, filePath, fileTitle, linkText)
+		fileLinks[index] = filePath
 	}
 
 	return fileLinks
