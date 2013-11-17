0ca198f3548ee4478da6e2c01b4784fdf3b4ad1a
Andreas Koch <andyk7@gmail.com>
2013-11-17 20:19:58 +0100

    Changed the field wiring of messages so the message itself is displayed on overview pages and the title is just the date  ... maybe I'll remove messages alltogether. It doesn't make to much sense to support them here.

--- parser/message/message.go
+++ parser/message/message.go
@@ -5,8 +5,6 @@
 package message
 
 import (
-	"fmt"
-	"github.com/andreaskoch/allmark/parser/pattern"
 	"github.com/andreaskoch/allmark/repository"
 	"strings"
 	"time"
@@ -20,32 +18,13 @@ func Parse(item *repository.Item, lines []string, fallbackTitle string) (sucess 
 		item.Title = fallbackTitle
 	}
 
-	// description
-	item.Description = getDescription(item)
-
-	// raw markdown content
-	item.RawContent = strings.TrimSpace(strings.Join(lines, "\n"))
+	messageContent := strings.TrimSpace(strings.Join(lines, "\n"))
+	item.Description = messageContent
 
 	return true, nil
 }
 
-func getDescription(item *repository.Item) string {
-	return item.MetaData.CreationDate.Format(time.RFC850)
-}
-
 func getTitle(item *repository.Item) string {
-	text := pattern.HtmlTagPattern.ReplaceAllString(item.RawContent, "")
-	excerpt := getTextExcerpt(text, 30)
 	time := item.MetaData.CreationDate.Format(time.RFC850)
-
-	return fmt.Sprintf("%s: %s", time, excerpt)
-}
-
-func getTextExcerpt(text string, length int) string {
-
-	if len(text) <= length {
-		return text
-	}
-
-	return text[0:length] + " ..."
+	return time
 }
