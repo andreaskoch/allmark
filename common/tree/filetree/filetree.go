42f423a28cb2e0095c56acd50e7e68a6198e6fd9
Andreas Koch <andyk7@gmail.com>
2014-05-11 14:36:21 +0200

    Changed the type of the new file tree type

--- common/tree/filetree/filetree.go
+++ common/tree/filetree/filetree.go
@@ -7,41 +7,42 @@ package filetree
 import (
 	"github.com/andreaskoch/allmark2/common/route"
 	"github.com/andreaskoch/allmark2/common/tree"
+	"github.com/andreaskoch/allmark2/common/tree/treeutil"
 	"github.com/andreaskoch/allmark2/model"
 )
 
-func New() *ItemTree {
-	return &ItemTree{
+func New() *FileTree {
+	return &FileTree{
 		*tree.New("", nil),
 	}
 }
 
-type ItemTree struct {
+type FileTree struct {
 	tree.Tree
 }
 
-func (nodeTree *ItemTree) Root() *model.Item {
+func (nodeTree *FileTree) Root() *model.File {
 	rootNode := nodeTree.Tree.Root()
 	if rootNode == nil {
 		return nil
 	}
 
-	return nodeToItem(rootNode)
+	return nodeToFile(rootNode)
 }
 
-func (nodeTree *ItemTree) InsertItem(item *model.Item) {
+func (nodeTree *FileTree) InsertFile(file *model.File) {
 
-	if item == nil {
+	if file == nil {
 		return
 	}
 
 	// convert the route to a path
-	path := routeToPath(item.Route())
+	path := treeutil.RouteToPath(file.Route())
 
-	nodeTree.Tree.Insert(path, item)
+	nodeTree.Tree.Insert(path, file)
 }
 
-func (nodeTree *ItemTree) GetItem(route *route.Route) *model.Item {
+func (nodeTree *FileTree) GetFile(route *route.Route) *model.File {
 
 	// locate the node
 	node := nodeTree.getNode(route)
@@ -50,35 +51,35 @@ func (nodeTree *ItemTree) GetItem(route *route.Route) *model.Item {
 	}
 
 	// cast the value
-	return nodeToItem(node)
+	return nodeToFile(node)
 }
 
-func (nodeTree *ItemTree) GetChildItems(route *route.Route) []*model.Item {
+func (nodeTree *FileTree) GetChildFiles(route *route.Route) []*model.File {
 
-	childItems := make([]*model.Item, 0)
+	childFiles := make([]*model.File, 0)
 
 	node := nodeTree.getNode(route)
 	if node == nil {
-		return childItems
+		return childFiles
 	}
 
 	for _, childNode := range node.Childs() {
-		if item := nodeToItem(childNode); item != nil {
-			childItems = append(childItems, item)
+		if file := nodeToFile(childNode); file != nil {
+			childFiles = append(childFiles, file)
 		}
 	}
 
-	return childItems
+	return childFiles
 }
 
-func (nodeTree *ItemTree) getNode(route *route.Route) *tree.Node {
+func (nodeTree *FileTree) getNode(route *route.Route) *tree.Node {
 
 	if route == nil {
 		return nil
 	}
 
 	// convert the route to a path
-	path := routeToPath(route)
+	path := treeutil.RouteToPath(route)
 
 	// locate the node
 	node := nodeTree.Tree.GetNode(path)
@@ -89,14 +90,6 @@ func (nodeTree *ItemTree) getNode(route *route.Route) *tree.Node {
 	return node
 }
 
-func nodeToItem(node *tree.Node) *model.Item {
-	return node.Value().(*model.Item)
-}
-
-func routeToPath(route *route.Route) tree.Path {
-	if route == nil {
-		return tree.NewPath()
-	}
-
-	return tree.NewPath(route.Components()...)
+func nodeToFile(node *tree.Node) *model.File {
+	return node.Value().(*model.File)
 }
