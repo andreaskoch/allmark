ff2c2fd266a6fa47fdac38c9f4812d7e3fa01741
Andreas Koch <andyk7@gmail.com>
2015-06-17 21:58:58 +0200

    Don't cache the breadcrumb navigation

--- src/allmark.io/modules/web/orchestrator/navigation.go
+++ src/allmark.io/modules/web/orchestrator/navigation.go
@@ -12,8 +12,7 @@ import (
 type NavigationOrchestrator struct {
 	*Orchestrator
 
-	breadcrumbNavigationByRoute map[string]*viewmodel.BreadcrumbNavigation
-	toplevelNavigation          *viewmodel.ToplevelNavigation
+	toplevelNavigation *viewmodel.ToplevelNavigation
 }
 
 func (orchestrator *NavigationOrchestrator) GetToplevelNavigation() *viewmodel.ToplevelNavigation {
@@ -52,71 +51,42 @@ func (orchestrator *NavigationOrchestrator) GetToplevelNavigation() *viewmodel.T
 	return orchestrator.GetToplevelNavigation()
 }
 
-func (orchestrator *NavigationOrchestrator) GetBreadcrumbNavigation(itemRoute route.Route) *viewmodel.BreadcrumbNavigation {
+func (orchestrator *NavigationOrchestrator) GetBreadcrumbNavigation(route route.Route) *viewmodel.BreadcrumbNavigation {
 
-	// return from cache if possible
-	if orchestrator.breadcrumbNavigationByRoute != nil {
-		return orchestrator.breadcrumbNavigationByRoute[itemRoute.String()]
+	// create a new bread crumb navigation
+	navigation := &viewmodel.BreadcrumbNavigation{
+		Entries: make([]*viewmodel.Breadcrumb, 0),
 	}
 
-	// initialize the cache
-	orchestrator.breadcrumbNavigationByRoute = make(map[string]*viewmodel.BreadcrumbNavigation)
-
-	// updateBreadcrumbNavigation writes a breadcrumb navigation model for the given route into the cache.
-	updateBreadcrumbNavigation := func(route route.Route) {
-
-		// create a new bread crumb navigation
-		navigation := &viewmodel.BreadcrumbNavigation{
-			Entries: make([]*viewmodel.Breadcrumb, 0),
-		}
-
-		// get the item for the supplied route
-		item := orchestrator.getItem(route)
-		if item == nil {
-			orchestrator.logger.Error("Returning an empty navigation model because there is no item for route %q.", route)
-			return
-		}
-
-		// recurse if there is a parent
-		if parent := orchestrator.getParent(item.Route()); parent != nil {
-			navigation.Entries = append(navigation.Entries, orchestrator.GetBreadcrumbNavigation(parent.Route()).Entries...)
-		}
-
-		// append a new navigation entry and return it
-		navigation.Entries = append(navigation.Entries, &viewmodel.Breadcrumb{
-			Title: item.Title,
-			Level: item.Route().Level(),
-			Path:  orchestrator.itemPather().Path(item.Route().Value()),
-		})
-
-		// mark the entries
-		for index, entry := range navigation.Entries {
-			if index < (len(navigation.Entries) - 1) {
-				entry.IsLast = false
-			} else {
-				entry.IsLast = true
-			}
-		}
-
-		orchestrator.breadcrumbNavigationByRoute[item.Route().String()] = navigation
+	// get the item for the supplied route
+	item := orchestrator.getItem(route)
+	if item == nil {
+		orchestrator.logger.Error("Returning an empty navigation model because there is no item for route %q.", route)
+		return navigation
 	}
 
-	// deleteBreadcrumbNavigation removes the cache entry for the given route.
-	deleteBreadcrumbNavigation := func(route route.Route) {
-		delete(orchestrator.breadcrumbNavigationByRoute, route.String())
+	// recurse if there is a parent
+	if parent := orchestrator.getParent(item.Route()); parent != nil {
+		navigation.Entries = append(navigation.Entries, orchestrator.GetBreadcrumbNavigation(parent.Route()).Entries...)
 	}
 
-	// write cache for all routes
-	for _, childRoute := range orchestrator.repository.Routes() {
-		updateBreadcrumbNavigation(childRoute)
+	// append a new navigation entry and return it
+	navigation.Entries = append(navigation.Entries, &viewmodel.Breadcrumb{
+		Title: item.Title,
+		Level: item.Route().Level(),
+		Path:  orchestrator.itemPather().Path(item.Route().Value()),
+	})
+
+	// mark the entries
+	for index, entry := range navigation.Entries {
+		if index < (len(navigation.Entries) - 1) {
+			entry.IsLast = false
+		} else {
+			entry.IsLast = true
+		}
 	}
 
-	// register update callbacks
-	orchestrator.registerUpdateCallback("update breadcrumb navigation", UpdateTypeNew, updateBreadcrumbNavigation)
-	orchestrator.registerUpdateCallback("update breadcrumb navigation", UpdateTypeModified, updateBreadcrumbNavigation)
-	orchestrator.registerUpdateCallback("update breadcrumb navigation", UpdateTypeDeleted, deleteBreadcrumbNavigation)
-
-	return orchestrator.GetBreadcrumbNavigation(itemRoute)
+	return navigation
 }
 
 func (orchestrator *NavigationOrchestrator) GetItemNavigation(route route.Route) *viewmodel.ItemNavigation {
